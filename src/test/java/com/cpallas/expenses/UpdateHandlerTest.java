f576836e0881d97352aed2e8e75a0490233b4670
Nail Mukminov <122352630+nmcpallas@users.noreply.github.com>
2026-01-10 15:13:21 +0400

    fixed problem with formatting  %.1f
    * undisabled checkStatus
    
    * fixed problem with formatting  %.1f

--- src/test/java/com/cpallas/expenses/UpdateHandlerTest.java
+++ src/test/java/com/cpallas/expenses/UpdateHandlerTest.java
@@ -10,7 +10,6 @@ import com.cpallas.expenses.storage.ids.UserId;
 import com.cpallas.expenses.storage.jpa.CategoryJpa;
 import com.cpallas.expenses.storage.jpa.ChatJpa;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -236,7 +235,6 @@ public class UpdateHandlerTest {
     }
 
     @Test
-    @Disabled
     void checkStatus() throws TelegramApiException {
         Mockito.when(telegramClient.execute(Mockito.any(AnswerCallbackQuery.class))).thenReturn(null);
         Mockito.when(expenseService.getStatus(Mockito.any(ChatId.class), Mockito.any(UserId.class))).thenReturn(SpendingStatus.builder().spent(100.0).income(100.0).build());
