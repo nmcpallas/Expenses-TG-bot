2f0603d5391789f5850e8b094eb4d64a312f1003
Nail Mukminov <122352630+nmcpallas@users.noreply.github.com>
2026-04-19 12:57:11 +0400

    fixed SpendingStatus.getStatus
    * fixed SpendingStatus.getStatus
    
    * fixed tests

--- src/main/java/com/cpallas/expenses/controller/dto/SpendingStatus.java
+++ src/main/java/com/cpallas/expenses/controller/dto/SpendingStatus.java
@@ -4,18 +4,18 @@ import java.math.BigDecimal;
 import java.util.Locale;
 import java.util.Map;
 
-public record SpendingStatus(BigDecimal spent,
-        BigDecimal income,
-        Map<String, BigDecimal> spedingByCategories) {
+public record SpendingStatus(BigDecimal monthLimit,
+        BigDecimal spent,
+        Map<String, BigDecimal> spendingByCategories) {
 
     public String getStatus() {
         StringBuilder sb = new StringBuilder();
 
         sb.append(String.format(
                 Locale.FRANCE,
-                "Потрачено: %.1f Сумма трат на месяц: %.1f, Остаток в этом месяце: %.1f\n",
-                spent, income, income.subtract(spent)));
-        spedingByCategories.entrySet().stream()
+                "Месячное ограничение: %.1f Потрачено на данный момент: %.1f, Остаток в этом месяце: %.1f\n",
+                monthLimit, spent, monthLimit.subtract(spent)));
+        spendingByCategories.entrySet().stream()
                 .sorted(Map.Entry.<String, BigDecimal>comparingByValue().reversed())
                 .forEach(entry ->
                         sb.append(String.format(Locale.FRANCE, "%s: %.1f\n", entry.getKey(), entry.getValue()))
