71ea1e68e8af813d5f80dcc08ab25f96403365f7
nmcpallas <nmcpallas@gmail.com>
2026-07-27 17:49:18 +0500

    fix

--- src/main/java/com/cpallas/expenses/storage/repo/ExpenseRepo.java
+++ src/main/java/com/cpallas/expenses/storage/repo/ExpenseRepo.java
@@ -4,6 +4,7 @@ import com.cpallas.expenses.storage.jpa.ExpenseJpa;
 import com.cpallas.expenses.storage.ids.ChatId;
 import com.cpallas.expenses.storage.ids.ExpenseId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
@@ -13,8 +14,10 @@ import java.util.Optional;
 
 public interface ExpenseRepo extends JpaRepository<ExpenseJpa, ExpenseId>, QuerydslPredicateExecutor<ExpenseJpa> {
 
+    @EntityGraph(attributePaths = "category")
     Optional<ExpenseJpa> findFirstByChat_IdOrderByCreatedAtDesc(ChatId chatId);
 
+    @EntityGraph(attributePaths = "category")
     Optional<ExpenseJpa> findByIdAndChat_Id(ExpenseId id, ChatId chatId);
 
     @Query("""
